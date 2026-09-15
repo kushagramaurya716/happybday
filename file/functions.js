a7588ab08dc8d17b93904b10fc38ff1d94133881
kushagramaurya716 <kushagramaurya716@gmail.com>
2026-09-15 14:43:08 +0530

    Add files via upload

--- file/functions.js
+++ file/functions.js
@@ -36,27 +36,27 @@ $(window).resize(function() {
 })(jQuery);
 
 function timeElapse(date){
-	var current = Date();
-	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
+	var current = new Date();
+	var seconds = (current.getTime() - date.getTime()) / 1000;
+	if (seconds < 0) seconds = 0;
+
 	var days = Math.floor(seconds / (3600 * 24));
 	seconds = seconds % (3600 * 24);
 	var hours = Math.floor(seconds / 3600);
-	if (hours < 10) {
-		hours = "0" + hours;
-	}
 	seconds = seconds % 3600;
 	var minutes = Math.floor(seconds / 60);
-	if (minutes < 10) {
-		minutes = "0" + minutes;
-	}
-	seconds = seconds % 60;
-	if (seconds < 10) {
-		seconds = "0" + seconds;
-	}
-	var result = "Days <span class=\"digit\">" + days + "</span> Hours <span class=\"digit\">" + hours + "</span> Minutes <span class=\"digit\">" + minutes; 
+	seconds = Math.floor(seconds % 60);
+
+	if (hours < 10) hours = "0" + hours;
+	if (minutes < 10) minutes = "0" + minutes;
+	if (seconds < 10) seconds = "0" + seconds;
+
+	var result = "Days <span class=\"digit\">" + days +
+		"</span> Hours <span class=\"digit\">" + hours +
+		"</span> Minutes <span class=\"digit\">" + minutes +
+		"</span> Seconds <span class=\"digit\">" + seconds + "</span>";
 	$("#clock").html(result);
 
 	var text = "THE WORLD JUST GOT LUCKIER SINCE ";
 	$("#message-box").html(text);
-
 }
